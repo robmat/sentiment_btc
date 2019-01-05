401d4c8f0ad4524276155826ebe0c0b657c5c65f
Bator <robbator@gmail.com>
2019-01-05 20:37:36 +0100

    SentimentApi - done.

--- src/main/java/com/bator/google/SentimentApi.java
+++ src/main/java/com/bator/google/SentimentApi.java
@@ -1,25 +1,94 @@
 package com.bator.google;
 
-import java.io.FileInputStream;
+import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
-import com.google.cloud.language.v1beta2.LanguageServiceClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
 
 public class SentimentApi {
 
+    private static final Logger log = Logger.getLogger(SentimentApi.class);
+
     Properties properties = new Properties();
 
-    public SentimentApi() throws IOException {
-        try (InputStream inputStream = getClass().getResourceAsStream("/resources/private.properties")){
+    public SentimentApi() {
+        try (InputStream inputStream = getClass().getResourceAsStream("/private.properties")) {
             properties.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException("IOException", e);
         }
     }
 
-    public void sentiment(String text) throws IOException {
-        LanguageServiceClient languageServiceClient = LanguageServiceClient.create();
+    public AnalyzeSentimentResponse sentiment(String text) throws IOException {
+        URL url = new URL("https://language.googleapis.com/v1/documents:analyzeSentiment?key=" + properties.getProperty("google.natural.language.api.key"));
+        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setDoOutput(true);
+        String json = new ObjectMapper().writeValueAsString(new AnalyzeSentimentRequest(text));
+        IOUtils.write(json, conn.getOutputStream());
+        int response = conn.getResponseCode();
+        if (response == 200) {
+            String resultString = IOUtils.toString(conn.getInputStream());
+            log.debug(resultString);
+            return new ObjectMapper().readValue(resultString, AnalyzeSentimentResponse.class);
+        } else {
+            String result = IOUtils.toString(conn.getErrorStream());
+            throw new RuntimeException(result);
+        }
+    }
+
+    @Data
+    public static class AnalyzeSentimentRequest {
+        public Document document;
+        public String encodingType = "UTF8";
+
+        AnalyzeSentimentRequest(String text) {
+            this.document = new Document(text);
+        }
+    }
+
+    @Data
+    public static class AnalyzeSentimentResponse {
+        DocumentSentiment documentSentiment;
+        String language;
+        List<Sentence> sentences;
+    }
+
+    @Data
+    public static class DocumentSentiment {
+        BigDecimal magnitude;
+        BigDecimal score;
+    }
+
+    @Data
+    public static class Sentence {
+        Text text;
+        DocumentSentiment sentiment;
+    }
+
+    @Data
+    public static class Text {
+        String content;
+        Integer beginOffset;
+    }
+    @Data
+    public static class Document {
+        public String content;
+        public String type = "PLAIN_TEXT";
+
+        Document(String text) {
+            content = text;
+        }
     }
 }
--- src/test/java/com/bator/google/SentimentApiTest.java
+++ src/test/java/com/bator/google/SentimentApiTest.java
@@ -1,8 +1,29 @@
 package com.bator.google;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
+
+import com.bator.google.SentimentApi.AnalyzeSentimentResponse;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class SentimentApiTest {
 
+    private SentimentApi sentimentApi = new SentimentApi();
 
+    @Test
+    public void shouldSentiment() throws IOException {
+        AnalyzeSentimentResponse sentiment = sentimentApi.sentiment("it feels great to have bitcoin, i'm very glad i've invested, to the moon!");
+        assertEquals(sentiment.getDocumentSentiment().getMagnitude().signum(), 1);
+        assertEquals(sentiment.getDocumentSentiment().getScore().signum(), 1);
+        assertEquals(sentiment.getLanguage(), "en");
+        assertEquals(sentiment.getSentences().size(), 1);
+
+        sentiment = sentimentApi.sentiment("it feels horrible to have bitcoin, i'm very sad i've invested, I quit!");
+        assertEquals(sentiment.getDocumentSentiment().getMagnitude().signum(), 1);
+        assertEquals(sentiment.getDocumentSentiment().getScore().signum(), -1);
+        assertEquals(sentiment.getLanguage(), "en");
+        assertEquals(sentiment.getSentences().size(), 1);
+    }
 }
